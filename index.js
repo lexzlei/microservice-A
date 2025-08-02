0a0a336d6cf2991ccdf9dfeaafbe2f0a99b0c9d7
lexzlei <lexleims@gmail.com>
2025-08-01 20:52:38 -0400

    added status message on root endpoint

--- index.js
+++ index.js
@@ -30,6 +30,8 @@ app.post('/safety-tips', async (req, res) => {
     }
 });
 
+app.get('/', (req, res) => res.send("Server is running"));
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
