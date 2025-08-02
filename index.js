0304e47cb02df641e39a235b45c5e84d2ea69d7b
lexzlei <lexleims@gmail.com>
2025-08-01 20:39:28 -0400

    Fix filname casing for render

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import generateSafetyTips from './GeminiService.js'; 
+import generateSafetyTips from './GeminiService.js';
 
 dotenv.config();
 const app = express();
